3229b9e3ad0fd52abc4dda89252c4ca60a3619d7
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-05-18 07:38:21 -0700

    Add work May 18, 24

--- app.js
+++ app.js
@@ -1,3 +1,78 @@
+// Topic : enum + loop                            MAY 18 , 24
+// enum Color {
+//     Red,
+//     Green,
+//     Yellow
+// }
+// let trafficLight : Color = Color.Green; 
+// console.log(Color.Yellow);           // result 2
+// console.log(Color[0]);               // result Red
+// console.log(Color['Yellow']);        // result 2
+// console.log(trafficLight);           // result 1
+// initialization enum
+// enum Color {
+//     Red = 2,
+//     Green,
+//     Yellow
+// }
+// console.log(Color.Green);               // result 3
+// console.log(Color.Yellow);              // result 4
+// console.log(Color.Red);                 // result 2
+// enum Color {
+//     Red,
+//     Green = 2,
+//     Yellow
+// }
+// console.log(Color.Yellow);                // result 3
+// console.log(Color.Green);                 // result 2
+// console.log(Color.Red);                   // result 0
+// real world example
+// enum Login {
+//     Google,
+//     Facebook,
+//     Email
+// }
+// enum are alternate of type literals or union
+// enum are used for data type or value
+// enum Color {
+//     Red,
+//     Green,
+//     Yellow
+// }
+// let trafficLight = Color.Red;
+// if( trafficLight == Color.Red) {
+//     console.log('Please Stop Vehicle!');
+// }
+// else if (trafficLight == Color.Yellow) {
+//     console.log('Start the engine and ready for drive');
+// }
+// else if (trafficLight == Color.Green) {
+//     console.log('Go!');
+// }
+// Home Work : enum example with switch statements / cases
+// loop
+// for(let i = 0; i < 100; i++) {
+//     console.log('Sorry!');
+// }
+// for (let i = 0; i < 3; i++) {
+//     console.log('Sorry!');
+// }
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// for(let i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+var fruits = ['Apple', 'Mango', 'Banana'];
+for (var i = 0; i <= 2; i++) {
+    console.log(fruits[1]);
+}
+// for(let i = 0; i <= 2; i++) {
+//     console.log(fruits[i]);
+// }
+// for(let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
 // Book: TypeScript Mastary a step-by-step learning experience
 // Variables
 // let favoriteColor = "red";
@@ -103,9 +178,9 @@ You can follow me at "http://linktr.ee/ameenalam"
 // let result = 3 + 4 * 5;
 // console.log(result);     // 23
 // answer will be 23 or 35 or ??
-var result = 3 + 4 * 5;
-result++;
-console.log(result); // 24
+// let result = 3 + 4 * 5;
+// result++;
+// console.log(result);   // 24
 // answer will be 23 or 24 or 35 or 36 or ??
 // 16-03-24 class 7
 // let userPercentage = 90;
