41a5b15dc71bbe94857bf4139f62bd0eee6acaee
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-06 14:48:48 -0700

    Add work on July 07, 2024

--- s06_Assignments/app.js
+++ s06_Assignments/app.js
@@ -1,10 +1,7 @@
 // Saturday                                        July 07, 2024
-
 // Topic : Logic building
-
 // Warmup : 1. Create a function sum()       2. it accepts two parameters
 //  3. add and return the answer    4. Print the answer outside the function
-
 /*
 const sum = (num1: number, num2: number) => {
     return num1 + num2;
@@ -14,9 +11,6 @@ const answer = sum(8, 8);
 
 console.log(answer);
 */
-
-
-
 /*
 const sum = (num1: number, num2: number) => {
     return num1 + num2;
@@ -26,15 +20,8 @@ const answer = sum(5, 5);
 
 console.log(answer);
 */
-
-
-
-
-
 // q1 : Write a function checkEvenOdd that accepts a number and prints 
 //      whether it is even or odd.
-
-
 /*
 let checkEvenOdd = (num: number) => {
     if(num % 2 === 0) {
@@ -47,28 +34,14 @@ let checkEvenOdd = (num: number) => {
 
 checkEvenOdd(4);
 */
-
-
-
-
-
-
 // q3 : Write a function arrayOperations that creates an array of numbers
 //      [4, 8, 6, 12, 28] , adds a number to the end of the array, removes 
 //      the first number and removes the middle number i.e: 6
-
-
-/*
-const arrayOperations = () => {
-    const myArr: number[] = [4, 8, 6, 12, 28];
-
-    myArr.push(5);       // [4, 8, 6, 12, 28, 5]
-    myArr.shift();       // [8, 6, 12, 28, 5]
-    myArr.splice(1, 1);  // [8, 12, 28, 5]
-    
+var arrayOperations = function () {
+    var myArr = [4, 8, 6, 12, 28];
+    myArr.push(5); // [4, 8, 6, 12, 28, 5]
+    myArr.shift(); // [8, 6, 12, 28, 5]
+    myArr.splice(1, 1); // [8, 12, 28, 5]
     console.log(myArr);
-    
-}
-
+};
 arrayOperations();
-*/
