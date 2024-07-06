a3e910f7baa821120236d15acb8908cbc5b980ca
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-06 14:09:28 -0700

    Keep working on date July 07, 2024

---
+++ s06_Assignments/app.ts
@@ -0,0 +1,7 @@
+// Saturday                                        July 07, 2024
+
+// Topic : Logic building
+
+// Warmup : 1. Create a function sum()       2. it accepts two parameters
+//  3. add and return the answer    4. Print the answer outside the function
+
