cd57d4722babc297690b29ab87c8603f631473cb
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-21 09:41:18 -0700

    Keep studying on July 21, 2024

---
+++ s08_w3tsmcqs/app.ts
@@ -0,0 +1,239 @@
+// MCQ's                                              July 21, 2024
+
+
+// q1: What are the three main 'simple types' in TypeScript
+
+// a. Object, String, Number          b. Object, Array, Symbol
+// c. Array, Object, Boolean          d. Boolean, Number, String 🟢
+
+
+
+
+
+// q2: What type of assignment is this variable, `const fullName: 
+//      string = 'Dylan Israel';`?
+
+// a. Explicit 🟢             b. Implicit 
+
+
+
+
+
+// q3: True or False: TypeScript can always correctly infer a variables
+//      type.
+
+// a. True  🟢                  b. False
+
+
+
+
+
+// q4: You can disable implicit variable type assignment by enabling 
+//      the compiler option:
+
+// a. noImplicitAny  🟢         b. Implicit = FALSE
+// c. noAutoType                 d. autoTypeAssignment = FALSE
+
+
+
+
+
+// q5: You can enable 'undefined' and 'null' types to be accounted for 
+//       by enabling the compiler property:
+
+// a. strictNullChecks 🟢        b. strictChecksRequired
+// c. noFalseyInits               d. noAutoType
+
+
+
+
+
+// q6: ______ is similar to 'any', but a safer alternative when 
+//       uncertain about the type.
+
+// a. unknown 🟢           b. never           c. similar
+
+
+
+
+
+// q7: What is the inherited type for the variable example in 
+//       `const example = ['Dylan']`?
+
+// a. string[]  🟢                   b. unknown[]
+// c. string                          d. any[]
+
+
+
+
+
+// q8: What does the 'readonly' access modifier do for an array 
+//       variable assignment like: 
+//     `const codeNames: readonly string[] = ['Coding', 'God']`?
+
+// a. Makes you read it for better clean code.
+
+// b. Allows no changes and is there simply to be read from and not 
+//      modified.  🟢
+
+// c. Allows only adding but not deleting elements in an array.
+
+// d. Makes it private and can only be used in the file is created.
+
+
+
+
+
+// q9: True or False: TypeScript will always correctly infer the type 
+//      of an array.
+
+// a. true 🟢               b. false
+
+
+
+
+
+// q10 : True or False: a Tuple and an Array are the same thing when 
+//          discussing types
+
+// a. True                     b. False  🟢
+
+
+
+
+
+// q11 : Which is a successful example of this tuple `[number, string]`
+
+// a. const ourTuple = [101, 'Coding God']  🟢
+// b. const ourTuple = [101]
+// c. const ourTuple = ['Coding God', 101]
+// d. const ourTuple = ['Coding God']
+
+
+
+
+
+// q12: Type Aliases are mostly used with ______.
+
+// a. Strings           b. Booleans  🟢            c. Numbers
+
+
+
+
+
+// q13: True or False: Interfaces are similar to type aliases, but 
+//         only for object types?
+
+// a. True 🟢                    b. False
+
+
+
+
+
+// q14: ________ an interface will have the same properties as that interface.
+
+// a. Improving                  b. Idolizing
+// c. Extending  🟢              d. Duplicating
+
+
+
+
+
+// q15: What are the two types of enums?
+
+// a. Number and Number Array          b. String and Boolean
+// c. String and Number 🟢             d. Number and Boolean
+
+
+
+
+
+// q16: Numeric enums first value is defaulted to what?
+
+// a. 5           b. 0 🟢              c. 1            d. 10
+
+
+
+
+
+// q17: True or False: 'keyof' can be used with index signatures to 
+//        extract the index type.
+
+// a. True                      b. False   🟢
+
+
+
+
+
+// q18: True or False: Generics can not be assigned default values
+
+// a. False                 b. True  🟢
+
+
+
+
+
+// q19: Definitely Typed is...
+
+// a. a project whose goal is to make types dynamic 
+
+// b. a project that provides a central repositary of TypeScript definitions
+//     for NPM packages which do not have types.  🟢
+
+// c. a superset of TypeScript      d. the official name of TypeScript
+
+
+
+
+
+// q20: What is the type of the parameter: 
+//          `function ex(param1?: string){}`?
+
+// a. string                            b. string | null
+// c. unknown                           d. string | undefined  🟢
+
+
+
+
+
+// q21: _____ is a return type for when nothing is returned
+
+// a. any                      b. any[]
+// c. void  🟢                   d. unknown
+
+
+
+
+
+// q22: Access modifiers control the ______ of properties and methods.
+
+// a. type  🟢                     b. mocking
+// c. visiblity                  d. inheritance
+
+
+
+
+
+// q23: True or False: public modifiers allow access to the class 
+//            members from anywhere.
+
+// a. False                b. True  
+
+
+
+
+
+// q24: True or False: protected modifiers only are allowed in the 
+//         inherited class.
+
+// a True  🟢                          b. False
+
+
+
+
+
+// q25: When a class extends another class and replaces the members of 
+//         its parent it is called what?
+
+// a. overload                              b. override 🟢
+// c. extending                             d. inheriting
