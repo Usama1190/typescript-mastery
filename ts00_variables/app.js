74ad6c5b31df6e2bf7ccb72090acc3ba59f2a81d
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-02 11:48:59 -0700

    Keep learning on Aug 02, 24

---
+++ ts00_variables/app.js
@@ -0,0 +1,3 @@
+// TypeScript Mastery: A step by step learning exprience
+// Book: Essential TypeScript 5 (third edition by Adam Freeman(author))
+console.log('Hello, World!');
---
+++ ts00_variables/app.ts
@@ -0,0 +1,14 @@
+// TypeScript Mastery: A step by step learning exprience     (Part-1)
+// Book: Essential TypeScript 5 (third edition by Adam Freeman(author))
+
+
+/*
+console.log('Hello, World!');
+
+let message = 'Hello World';
+console.log(message);
+*/
+
+
+
+// TypeScript Mastery: A step by step learning exprience     (Part-1)
