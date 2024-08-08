6203c6dabb2c1ae1ecc24e8917f515f21469b18f
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-08 05:11:42 -0700

    Keep learning on Aug 08, 24

--- assignments/a08_questions/app.ts
+++ assignments/a08_questions/app.ts
@@ -38,3 +38,70 @@ function add(a: number, b: number): number {
 let add = (a: number, b: number): number => a + b;
 console.log(add(12, 23));
 */
+
+
+
+
+
+// 4. Implementing a Rest Parameter
+/*
+function usama(...rest: number[]) {
+    let usama = rest.reduce((a: number, b: number) => a + b);
+
+    console.log(usama);
+    
+}
+
+usama(1, 2, 3);
+*/
+
+
+
+
+
+// 5. Function Returning Another Function
+/*
+function hiUsama(first: number) {
+    let num1 = first;
+    function anotherFunc(second: number) {
+        let num2 = second;
+        let multiplier = num1 * num2;
+        return multiplier;
+        
+    }
+    return anotherFunc;
+}
+
+let myVar = hiUsama(2);
+console.log(myVar(2));
+*/
+
+/*
+function multiplier(arg1: number) {
+    function returnFunc(arg2: number) {
+        return arg1 * arg2;
+    }
+
+    return returnFunc;
+}
+
+const triple = multiplier(12);
+console.log(triple(2));
+*/
+
+
+
+
+
+// 6. Recursive Function - Factorial
+/*
+function factorial(n: number): number {
+    if( n <= 0) {
+        return 1;
+    }
+    return n * factorial(n - 1)
+}
+
+const result = factorial(5);
+console.log(result);
+*/
--- assignments/a08_questions/readme.md
+++ assignments/a08_questions/readme.md
@@ -34,4 +34,27 @@ function add(a: number, b: number): number {
 
 
 
-4. 
+4. Implementing a Rest Parameter
+Write a function sumAll that uses a rest parameter to take any number of arguments and return their sum.
+
+console.log(1, 2, 3);    // Should print 6
+console.log(10, 20, 30, 40, 50);    // Should print 150
+
+
+
+
+
+5. Function Returning Another Function
+Create a function multiplier that takes a number as its argument and return another function. The returned function should take a single number as its argument and return the product of its argument and the argument of the first function
+
+const triple = multiplier(3);
+console.log(triple(5));      // Should print 15
+
+
+
+
+
+6. Recursive Function - Factorial
+Write a recursive function to calculate the factorial of a number. The factorial of a number n is a product of all positive integers less than and eqaul to n. For example the factorial of 5 (5!) is 5*4*3*2*1 = 120
+
+console.log(factorial(5));    // Should print 120
