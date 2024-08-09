c581ac9e51b1452acfa59526441b9507154503e5
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-09 13:04:40 -0700

    Keep learning on Aug 10, 24

---
+++ assignments/a09_questions_p2/app.js
@@ -0,0 +1,41 @@
+// TypeScript Home Work Assignments
+// 1. Basic Array Operations
+/*
+let fruits: string[] = ["Apple", "Orange", "Banana", "Mango"];
+
+fruits.push('Watermalon');
+fruits.shift();
+fruits.unshift('PineApple');
+
+fruits.splice(2, 1);
+
+console.log(fruits);
+*/
+// 2. Working with Multidimentional Array
+let multiDiArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+function findDiagnol(a) {
+    let count = 0;
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a.length; j++) {
+            const element = a[i][j + count];
+            console.log(element);
+        }
+    }
+    ++count;
+}
+findDiagnol(multiDiArray);
+export {};
+/*
+function sumArr(a: number[][]) {
+    let myArr = a.flat();
+    let sum = myArr.reduce((a: number, b: number) => a + b);
+    return sum;
+}
+
+let returnValue = sumArr(multiDiArray);
+console.log(returnValue);
+*/ 
---
+++ assignments/a09_questions_p2/readme.md
@@ -0,0 +1,26 @@
+TypeScript Home Work Assignment
+
+
+
+
+1. Basic Array Operations
+Create an array called fruits that contains the names of four different fruits. Perform the follwoing operations:
+
+- Add a new fruit to the end of the array.
+- Remove the first fruit from the array.
+- Add a new fruit to the beginning of the array.
+- Find the index of a fruit and remove that fruit using the index.
+
+Example usage and operations on the "fruits" array.
+
+
+
+
+
+2. Working with Multidimentional Array
+Define a 3x3 matrix of numbers as a multidimentional array. Write functions to.
+
+- Print the diagnal elements of the matrix
+- Calculate the sum of all elements in the matrix
+
+Define the matrix and implements the functions
