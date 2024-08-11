b56c0c80d1c4e0d556f7597084c67179a445d0b0
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-11 05:35:34 -0700

    Keep learning on Aug 11, 24

--- assignments/a10-questions_p3/app.ts
+++ assignments/a10-questions_p3/app.ts
@@ -39,3 +39,116 @@ function printEachProperty() {
 
 printEachProperty();
 */
+
+
+/*
+const car = {
+    make: 'Toyota',
+    model: 'XC60',
+    year: 2000
+}
+
+function printEachProperty(car: { [key: string]: string | number }) {
+    for(const key in car) {
+        console.log(`Property: ${key}, and its value: ${car[key]}`);
+        
+    }
+}
+
+printEachProperty(car);
+*/
+
+
+
+
+
+
+// 3. Basic For Loop Exercise
+/*
+function myFunc() {
+    for (let i = 1; i <= 100; i++) {
+        const element = i;
+
+        if(element % 3 === 0) {
+            console.log('Fizz');
+            
+        }else if(element % 5 === 0) {
+            console.log('Buzz');
+            
+        }
+        else if(element % 3 === 0 && element % 5 === 0) {
+            console.log('FizzBuzz');
+            
+        }
+        else {
+            console.log(i);
+            
+        }
+        
+    }
+}
+
+myFunc();
+*/
+
+
+
+
+
+
+// 4. Practicing Do-While Loop
+/*
+function guessingGame() {
+    let userNum = prompt('Guess a number between 1 to 10');
+    let mechineGeneratedNumber: number = Math.random();
+    console.log(mechineGeneratedNumber);
+    
+}
+
+guessingGame();
+*/
+
+
+
+
+
+// 5. While Loop For a Countdown
+/*
+function countDown(num: number) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(15);
+*/
+
+
+
+
+
+// 6. Enums For Days of the Week
+/*
+function checkWeekDays(day: string) {
+    switch (day) {
+        case day = 'Sunday' || 'Saturday':
+            console.log('Weekend');
+            break;
+    
+        default:
+            console.log('Weekday');            
+    }
+}
+
+checkWeekDays('Sunday');
+*/
+
+
+
+
+// 7. Tuples For RGB Colors
+
+const tuple: [string, string, string] = ['Red', 'Green', 'Blue'];
+
+function rgb(x: []) {}
